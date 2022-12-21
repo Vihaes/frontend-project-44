f327d0cd4530c8935dc9b6f1c14f95044b42818d
vihaes <r.vihaes@gmail.com>
2022-12-21 12:56:43 +0300

    commit 0.9.2

---
+++ bin/brain-calc.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import name from './cli.js';
+
+const gameCalc = 'What is the result of the expression?';
+const sign = ['+', '-', '*'];
+
+let wonCounter = 0;
+for (let i = 0; i < 3; i += 1) {
+  const num1 = Math.floor(Math.random() * 10);
+  const num2 = Math.floor(Math.random() * 10);
+
+  const signRandomizer = Math.floor(Math.random() * sign.length);
+  const randomizedSign = sign[signRandomizer];
+
+  let sum = 0;
+  let symb = 0;
+  switch (randomizedSign) {
+    case '+':
+      sum = num1 + num2;
+      symb = '+';
+      break;
+    case '-':
+      sum = num1 - num2;
+      symb = '-';
+      break;
+    case '*':
+      sum = num1 * num2;
+      symb = '*';
+      break;
+    default:
+      console.log('');
+  }
+
+  sum = String(sum);
+  // console.log(typeof sum);
+
+  const question = readlineSync.question(`Question: ${num1} ${symb} ${num2} \nYour answer: `);
+  // console.log(typeof question);
+  if (sum === question) {
+    console.log('Correct!');
+    wonCounter += 1;
+  } else if (sum !== question) {
+    console.log(question + ' is wrong answer ;(. Correct answer was ' + sum + '\nLet\'s try again, Tom!');
+  }
+
+  if (wonCounter >= 3) {
+    console.log('Congratulations Sam, you are the champ!');
+  }
+}
+
+export default gameCalc;
