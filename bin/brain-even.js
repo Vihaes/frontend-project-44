1cdd485d8f253cea3cb21f816949fa0403a6e2d9
vihaes <r.vihaes@gmail.com>
2022-12-21 13:17:51 +0300

    commit 0.9.1

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,22 +4,24 @@ import name from './cli.js';
 
 const gameEven = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+let winCounter = 0;
 for (let i = 0; i < 3; i += 1) {
   const num = Math.floor(Math.random() * 100);
   const question = readlineSync.question(`Question: ${num} \nYour answer: `);
 
   if (num % 2 === 0 && question === 'yes') {
     console.log('Correct!');
+    winCounter += 1;
   } else if (num % 2 === 1 && question === 'no') {
     console.log('Correct!');
+    winCounter += 1;
   } else if (num % 2 === 1 && question === 'yes') {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no' \nLet's try again, Jimm!");
   } else if (num % 2 === 0 && question === 'no') {
     console.log("'no' is wrong answer ;(. Correct answer was 'yes' \nLet's try again, Tom!");
   }
 
-  const score = i + 1;
-  if (score === 3) {
+  if (winCounter >= 3) {
     console.log('Congratulations Sam, you are the champ!');
   }
 }
